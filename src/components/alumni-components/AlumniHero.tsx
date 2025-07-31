872430336e46af4eef97f763b678d4e3639ec126
eric2003tu <mukarugambwaelevanie@gmail.com>
2025-07-31 23:38:20 +0200

    started the alumni purelly

--- src/components/alumni-components/AlumniHero.tsx
+++ src/components/alumni-components/AlumniHero.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 const AlumniHero = () => {
   return (
-    <div className="h-full w-full lg:px-30 px-4">
+    <div className="h-full w-full ">
     <div style={{backgroundImage: `url(${'showcase-1.webp'})`}} className="w-full  h-64 text-white flex justify-center bg-cover bg-no-repeat">
-        <div className="w-full h-full pt-24  bg-gray-900/70 flex flex-col gap-2 text-center">
+        <div className="w-full h-full pt-24 lg:px-30 px-4  bg-gray-900/70 flex flex-col gap-2 text-center">
         <h1 className="font-bold text-4xl">Alumni</h1>
         <p>Esse dolorum voluptatum ullam est sint nemo et est ipsa porro placeat quibusdam quia assumenda numquam molestias.</p>
         <p>
