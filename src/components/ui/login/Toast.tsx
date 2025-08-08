1ffa02bfdc7636cc1662bb911f0176e233904370
eric2003tu <mukarugambwaelevanie@gmail.com>
2025-08-08 12:55:24 +0200

    Dashboard added now

--- src/components/ui/login/Toast.tsx
+++ src/components/ui/login/Toast.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MdClose } from 'react-icons/md';
 
 interface ToastProps {
   show: boolean;
@@ -36,9 +37,7 @@ export const Toast: React.FC<ToastProps> = ({ show, message, type, onClose }) =>
               <span className="text-sm font-medium">{message}</span>
             </div>
             <button onClick={onClose} className="ml-4 text-gray-400 hover:text-gray-600 transition-colors">
-              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-              </svg>
+              <MdClose size={25}/>
             </button>
           </div>
         </div>
